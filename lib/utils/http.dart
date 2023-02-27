f54379756da3af94f716731c2a30e6e57e433662
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2023-02-27 11:18:47 +0200

    Initial commit

--- lib/utils/http.dart
+++ lib/utils/http.dart
@@ -1,24 +1,23 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:xx/model/appmodel.dart';
 
 class HttpService {
   static String get baseUrl => "REPLACE_POST_URL";
 
-  //for post request
-  static Future<List<AppModel>> getDataResponse(
+  /// for post request
+  static Future<List<ExampleModel>> getDataResponse(
       {Map<String, dynamic>? body}) async {
-    //you can also pass the headers in post method.
+    /// you can also pass the headers in post method.
     http.Response res = await http.post(Uri.parse(baseUrl), body: body);
 
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
 
-      //replace your own json-model
-      List<AppModel> posts = body
+      /// replace your own json-model
+      List<ExampleModel> posts = body
           .map(
-            (dynamic item) => AppModel.fromJson(item),
+            (dynamic item) => ExampleModel.fromJson(item),
           )
           .toList();
 
@@ -28,24 +27,41 @@ class HttpService {
     }
   }
 
-  //for get request
-  static Future<List<AppModel>> getResponse() async {
+  /// for get request
+  static Future<List<ExampleModel>> getResponse() async {
     http.Response res = await http
         .get(Uri.parse(baseUrl), headers: {"Content-Type": "application/json"});
 
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
 
-      //replace your json-model
-      List<AppModel> users = body
-          .map(
-            (dynamic item) => AppModel.fromJson(item),
-          )
-          .toList();
+      /// replace your json-model
+      List<ExampleModel> users =
+          body.map((dynamic item) => ExampleModel.fromJson(item)).toList();
 
       return users;
     } else {
       throw "Can't get posts.";
     }
   }
+}
+
+class ExampleModel {
+  String? message;
+  int? status;
+
+  ExampleModel({
+    this.message = "",
+    this.status,
+  });
+
+  ExampleModel.fromJson(Map<String, dynamic> map) {
+    message = map['message'] ?? "";
+    status = map['status'] ?? 0;
+  }
+
+  Map<String, dynamic> toJson() => {
+        'message': message,
+        'status': status,
+      };
 }
