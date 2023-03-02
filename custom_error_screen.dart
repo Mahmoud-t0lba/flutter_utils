6d71261a29be492adb37a824783cc49cf8203f4b
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2023-03-02 10:16:52 +0200

    Update custom_error_screen.dart

--- custom_error_screen.dart
+++ custom_error_screen.dart
@@ -3,19 +3,16 @@ import 'package:futara/controller/constants/app_images.dart';
 import 'package:futara/controller/constants/size.dart';
 import 'package:futara/view/widgets/my_text.dart';
 
+/// TODO : just call it on main func
 customErrorScreen() {
   return ErrorWidget.builder = ((details) {
     return Material(
       child: Container(
         color: Colors.black87,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: [
-            Image.asset(
-              AppImages.logo,
-              width: 200,
-              height: 200,
-            ),
+         /// TODO : add your image
+            Image.asset(AppImages.logo, width: 200, height: 200),
             20.h,
             Padding(
               padding: const EdgeInsets.all(8),
