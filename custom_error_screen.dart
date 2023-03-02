215faeb9f395ed9f270a72b80ca95d25c626fbb8
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2023-03-02 10:14:21 +0200

    Add files via upload

---
+++ custom_error_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:futara/controller/constants/app_images.dart';
+import 'package:futara/controller/constants/size.dart';
+import 'package:futara/view/widgets/my_text.dart';
+
+customErrorScreen() {
+  return ErrorWidget.builder = ((details) {
+    return Material(
+      child: Container(
+        color: Colors.black87,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              AppImages.logo,
+              width: 200,
+              height: 200,
+            ),
+            20.h,
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: MyText(
+                title: details.exception.toString(),
+                color: Colors.white,
+                alien: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  });
+}
