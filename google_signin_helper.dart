b7bf82464c6afbd57cc4753c2278d2b343909080
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2023-04-06 03:13:56 +0200

    Update and rename google_sign_in_helper.dart to google_signin_helper.dart

---
+++ google_signin_helper.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: unnecessary_null_comparison
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInUtils {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<String> signInWithGoogle() async {
+    final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount!.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    UserCredential authResult = await _auth.signInWithCredential(credential);
+    final User? user = authResult.user;
+
+    assert(!user!.isAnonymous);
+    assert(await user!.getIdToken() != null);
+
+    User currentUser = _auth.currentUser!;
+    assert(user!.uid == currentUser.uid);
+
+    return 'signInWithGoogle succeeded: $user';
+  }
+
+  void signOutGoogle() async {
+    await googleSignIn.signOut();
+
+    print("User Sign Out");
+  }
+}
