a534c31b2a3f64d4deb6bb04d92fe26bc2a1ed16
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2023-06-25 17:29:18 +0300

    Create network_exceptions.dart

---
+++ network_exceptions.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:get/get.dart' hide Response;
+
+abstract class NetworkExceptions {
+  static String handleResponse(Response response) {
+    int statusCode = response?.statusCode ?? 0;
+    switch (statusCode) {
+      case 400:
+      case 401:
+      case 403:
+        // Navigate to login route here
+        return "Unauthorized Request";
+        break;
+      case 404:
+        return "Not found";
+        break;
+      case 409:
+        return "Error due to a conflict";
+        break;
+      case 408:
+        return "Connection request timeout";
+        break;
+      case 500:
+        return "Internal Server Error";
+        break;
+      case 503:
+        return "Service unavailable";
+        break;
+      default:
+        return "Received invalid status code";
+    }
+  }
+
+  static String getDioException(error) {
+    if (error is Exception) {
+      try {
+        var errorMessage = "";
+        if (error is DioError) {
+          switch (error.type) {
+            case DioErrorType.cancel:
+              errorMessage = "Request Cancelled";
+              break;
+            case DioErrorType.connectTimeout:
+              errorMessage = "Connection request timeout";
+              break;
+            case DioErrorType.other:
+              errorMessage = "No internet connection";
+              break;
+            case DioErrorType.receiveTimeout:
+              errorMessage = "Send timeout in connection with API server";
+              break;
+            case DioErrorType.response:
+              errorMessage = NetworkExceptions.handleResponse(error.response);
+              break;
+            case DioErrorType.sendTimeout:
+              errorMessage = "Send timeout in connection with API server";
+              break;
+          }
+        } else if (error is SocketException) {
+          errorMessage = "No internet connection";
+        } else {
+          errorMessage = "Unexpected error occurred";
+        }
+        return errorMessage;
+      } on FormatException {
+        return "Unexpected error occurred";
+      } catch (_) {
+        return "Unexpected error occurred";
+      }
+    } else {
+      if (error.toString().contains("is not a subtype of")) {
+        return "Unable to process the data";
+      } else {
+        return "Unexpected error occurred";
+      }
+    }
+  }
+}
