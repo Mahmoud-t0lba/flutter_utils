fc1da263cecac029490521b9e9632a11a20e8b9f
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2023-08-22 14:06:28 +0300

    Add files via upload

---
+++ size_config.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static BuildContext? appContext;
+  static MediaQueryData? _mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+  static Orientation? orientation;
+
+  static void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    appContext = context;
+    screenWidth = _mediaQueryData!.size.width;
+    screenHeight = _mediaQueryData!.size.height;
+    orientation = _mediaQueryData!.orientation;
+  }
+
+  static double getHeight(double height) {
+    return _mediaQueryData!.size.height * (height / 814);
+  }
+
+  static double getWidth(double width) {
+    return _mediaQueryData!.size.width * (width / 360);
+  }
+
+  static double getTextSize(double textSize) {
+    return _mediaQueryData!.size.width * (textSize / 414);
+  }
+}
